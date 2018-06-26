21c671b3f59fee174c17e1803c8e5f4052fdf2da
sheela <sheela19170@gmail.com>
2018-06-26 11:55:59 +0530

    Initial commit

---
+++ hr/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def hrview(request):
+    return render(request, '')
---
+++ student/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def studentview(request):
+    return render(request, '')
